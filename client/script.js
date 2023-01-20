89842c939baeba3424b18d14aaa0cebd788b4fab
alakh0703 <89909186+alakh0703@users.noreply.github.com>
2023-01-19 23:10:23 -0500

    Update script.js

--- client/script.js
+++ client/script.js
@@ -137,7 +137,7 @@ const handleSubmit = async (e) => {
   // messageDiv.innerHTML = "..."
   loader(messageDiv)
   // https://codex-im0y.onrender.com/
-  const response = await fetch('http://localhost:5000', {
+  const response = await fetch('https://chatgpt-clone-z3iv.onrender.com', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
